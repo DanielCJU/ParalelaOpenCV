d701382b454bdcc12f7a2dd1a82bc9b6cf69b07e
DanielCJU <51923424+DanielCJU@users.noreply.github.com>
2020-07-15 14:24:53 -0400

    Implementacion mpi

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <time.h>
+#include <ctime>
 #include <stdio.h>
+#include "mpi.h"
+#include <thread>
+#include <cstdlib>
+#include <chrono>
 
 using namespace cv;
 using namespace std;
@@ -35,14 +39,82 @@ int convertirStringEnEntero(std::string linea){
         return 666;
     }
 }
+
+void matsnd(const Mat& m,int dest){
+      int rows  = m.rows;
+      int cols  = m.cols;
+      int type  = m.type();
+      int channels = m.channels();
+      memcpy(&buffer[0 * sizeof(int)],(uchar*)&rows,sizeof(int));
+      memcpy(&buffer[1 * sizeof(int)],(uchar*)&cols,sizeof(int));
+      memcpy(&buffer[2 * sizeof(int)],(uchar*)&type,sizeof(int));
+	  cout << "matsnd: filas=" << rows << endl;
+      cout << "matsnd: columnas=" << cols << endl;
+      cout << "matsnd: type=" << type << endl;
+      cout << "matsnd: channels=" << channels << endl;
+      cout << "matsnd: bytes=" << bytes << endl;
+
+      int bytespersample=1;
+      int bytes=m.rows*m.cols*channels*bytespersample;
+	  
+      if(!m.isContinuous())
+      { 
+         m = m.clone();
+      }
+      memcpy(&buffer[3*sizeof(int)],m.data,bytes);
+      MPI_Send(&buffer,bytes+3*sizeof(int),MPI_UNSIGNED_CHAR,dest,0,MPI_COMM_WORLD);
+}
+
+Mat matrcv(int src){
+      MPI_Status status;
+      int count,rows,cols,type,channels;
+
+      MPI_Recv(&buffer,sizeof(buffer),MPI_UNSIGNED_CHAR,src,0,MPI_COMM_WORLD,&status);
+      MPI_Get_count(&status,MPI_UNSIGNED_CHAR,&count);
+      memcpy((uchar*)&rows,&buffer[0 * sizeof(int)], sizeof(int));
+      memcpy((uchar*)&cols,&buffer[1 * sizeof(int)], sizeof(int));
+      memcpy((uchar*)&type,&buffer[2 * sizeof(int)], sizeof(int));
+
+      cout << "matrcv: Count=" << count << endl;
+      cout << "matrcv: filas=" << rows << endl;
+      cout << "matrcv: columnas=" << cols << endl;
+      cout << "matrcv: type=" << type << endl;
+
+      Mat received= Mat(rows,cols,type,(uchar*)&buffer[3*sizeof(int)]);
+      return received;
+}
+
 int main( int argc, char** argv )
 {
+	int mi_rango; /* rango del proceso    */
+    int procesadores; /* numero de procesos   */
+    int maestro = 0; /* Identificador maestro */
+    MPI_Status estado; /* devuelve estado al recibir*/
+	
+	/* Este string se usara para detener los hilos paralelos */
+    std::string parar("STOP");
+	
     if(argc!=3){
         std::cout << "No se han entregado los argumentos necesarios. Se cerrará el programa" << std::endl;
         return EXIT_FAILURE;
     }
+        
+	/* Comienza las llamadas a MPI */
+    MPI_Init(&argc, &argv);
 
-    std::string ruta(argv[2]);
+    /* Averiguamos el rango de nuestro proceso */
+    MPI_Comm_rank(MPI_COMM_WORLD, &mi_rango);
+
+    /* Averiguamos el número de procesos que estan 
+    * ejecutando nuestro porgrama 
+    */
+    MPI_Comm_size(MPI_COMM_WORLD, &procesadores);
+	
+	if (procesadores < 2) {
+            fprintf(stderr, "\nLa implementación requiere al menos 2 procesadores\n");
+            return EXIT_FAILURE;
+        }
+		
     std::string opcion(argv[1]);
     std::cout << opcion << std::endl;
     int numero = convertirStringEnEntero(opcion);
@@ -50,23 +122,26 @@ int main( int argc, char** argv )
     if(numero==666){
         return EXIT_FAILURE;
     }
-
-    std::cout << ruta << std::endl;
-    cv::Mat imagenOriginal = imread(ruta);
-    
-    if (!imagenOriginal.data) {
-        return EXIT_FAILURE;
-    }
     
     if(numero==1){
-       cv::Mat imagenDifuminada;
-       cv::GaussianBlur(imagenOriginal, imagenDifuminada, cv::Size(3, 3), 0);
-       imagenOriginal.release();
-       char temporalUno[13];
-       std::string NombreUno = "operacion_1_"+convertirEnString(tiempoActual(temporalUno), 14)+".png";
-       imwrite(NombreUno, imagenDifuminada);
-       imagenDifuminada.release();
-       return 0; 
+	   if(mi_rango==0){
+		   Mat received=matrcv(1);
+		   char temporalUno[13];
+           std::string NombreUno = "operacion_1_"+convertirEnString(tiempoActual(temporalUno), 14)+".png";
+		   imwrite("NombreUno",received);
+		   received.release();
+		   return 0;
+	   } else {
+		   std::string ruta(argv[2]);
+		   cv::Mat imagenOriginal = imread(ruta);
+		   if (!imagenOriginal.data) {
+               return EXIT_FAILURE;
+           }
+		   cv::Mat imagenDifuminada;
+           cv::GaussianBlur(imagenOriginal, imagenDifuminada, cv::Size(3, 3), 0);
+           imagenOriginal.release();
+		   matsnd(imagenDifuminada,0);
+	   }
     }
 
     if(numero==2){
