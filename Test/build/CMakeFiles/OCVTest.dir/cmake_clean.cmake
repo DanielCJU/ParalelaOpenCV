cf8ae80dc484a4b06778135de1832a4c13d9ef5f
DanielCJU <daniel.cajasu@utem.cl>
2020-07-16 14:16:56 +0000

    Implementacion mpi: arreglos y pruebas

---
+++ Test/build/CMakeFiles/OCVTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OCVTest.dir/main.cpp.o"
+  "OCVTest"
+  "OCVTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OCVTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
