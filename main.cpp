3eae9c2b3f454bf072831eca44e36a3f41f7666b
Ricardo Aliste G <39688076+Reko-Glarius@users.noreply.github.com>
2020-08-12 23:53:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -482,7 +482,7 @@ int main(int argc, char** argv ){
                      recibir(imgtmpjoin, p);
                      join_luminosity_scale(imgtmpjoin, newimg2, p, procesadores);
                      newimg=newimg2.clone();
-                     imwrite((char)p, newimg); ///Se almacena la imagen resultante
+                     imwrite("A", newimg); ///Se almacena la imagen resultante
                  }
              }
              else ///Caso de no serlo
